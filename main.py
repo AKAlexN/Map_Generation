879a73bda15943a1aec0f72767cd1851e320b14b
AKAlexN <120011128+AKAlexN@users.noreply.github.com>
2023-02-24 21:34:25 +0300

    Add files via upload
    Код создающий карту из txt файла с данными полученными от лидара

---
+++ main.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+f = open(".\examp4.txt", 'r')
+data_raw = f.readlines()
+f.close()
+n = len(data_raw)
+data = np.zeros((n, 684))
+for i in range(n):
+    line = data_raw[i]
+# for line in data_raw:
+    a = line.replace(',', '')
+    a = a.replace(';', '')
+    a = a.split()
+    new_line = list(map(float,a))
+    # data.append(new_line)
+    data[i,:] = new_line
+
+x, y, angle = data[:,0], data[:,1], data[:,2]
+
+beta = 240.0/681
+
+theta = np.radians(np.linspace(-120, 121, 681))
+a, b, = [], []
+
+plt.figure(1)
+for i in range(n):
+    for j in range(3,684):
+        if 2.0 < data[i,j] < 5.6:
+            a.append(np.cos(angle[i]-theta[j])*(data[i,j])+x[i]+0.3*np.cos(angle[i]))
+            b.append(np.sin(angle[i]-theta[j])*(data[i,j])+y[i]+0.3*np.sin(angle[i]))
+
+plt.plot(x,y)
+plt.scatter(a,b, color="red");       
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
